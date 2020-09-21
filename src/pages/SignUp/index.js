35e32922df34379063e82bb3e9922a9c099b90eb
Chris Evans <evanschris@users.noreply.github.com>
2020-09-21 14:39:34 +0100

    add styles to sign in page

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,59 +1,15 @@
 import React, { Component } from 'react';
 import { Link, withRouter } from 'react-router-dom';
-import styled from "styled-components";
-import theme from "../../_theme";
 import { Button } from "../../components/Button";
 import { ButtonSecondary } from '../../components/ButtonSecondary';
 import { AuthUserContext } from '../Session';
 import SignOutButton from '../SignOut';
+import { Label, ErrorMessage } from '../../components/Forms/formsStyles';
 
 import { withFirebase } from '../Firebase';
 import * as ROUTES from '../../constants/routes';
 import * as ROLES from '../../constants/roles';
 import * as ERRORS from '../../constants/errors';
-
-const Label = styled.label`
-  font-weight: bold;
-  display: block;
-  margin-bottom: 15px;
-
-  input, select {
-    display: block;
-    width: 100%;
-    min-width: 400px;
-    margin-top: 5px;
-    padding: 5px;
-    font-size: 1rem;
-    border: 2px solid ${theme.black};
-    @media screen and (min-width: ${theme.m}){
-      width: auto;
-    }
-    &:hover {
-      border-color: rgba(${theme.black}, 0.7);
-    }
-    &:focus {
-      outline: none;
-      border-radius: 0;
-      box-shadow: 0px 0px 0px 3px ${theme.focus};
-    }
-  }
-  input {
-    width: calc(100% - 14px);
-    min-width: 386px;
-    @media screen and (min-width: ${theme.m}){
-      width: auto;
-    }
-  }
-  .hidden-field {
-    display: none;
-  }
-`
-
-const ErrorMessage = styled.p`
-  color: ${theme.red};
-  font-weight: bold;
-`
-
 
 const SignUpPage = () => (
   <div>
