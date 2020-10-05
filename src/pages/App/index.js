20fa3e58a208d2fc378a08a38a773cac6123c772
Chris Evans <evanschris@users.noreply.github.com>
2020-10-05 17:05:58 +0100

    try core js

--- src/pages/App/index.js
+++ src/pages/App/index.js
@@ -1,7 +1,4 @@
-import 'react-app-polyfill/ie9';
-import 'react-app-polyfill/ie11';
-import 'react-app-polyfill/stable';
-
+import 'core-js/stable'
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { ToastProvider } from 'react-toast-notifications'
