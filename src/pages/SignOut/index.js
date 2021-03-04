5ceea33a691265585028a5435cead6c45305bd98
Chris Evans <evanschris@users.noreply.github.com>
2021-03-04 14:48:06 +0000

    update copy

--- src/pages/SignOut/index.js
+++ src/pages/SignOut/index.js
@@ -6,7 +6,7 @@ import { withFirebase } from '../Firebase';
 
 const SignOutButton = ({ firebase }) => (
   <Button type="button" onClick={firebase.doSignOut}>
-    Sign Out of account
+    Sign out of account
   </Button>
 );
 
